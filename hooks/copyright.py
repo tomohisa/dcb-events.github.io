34af586c5de6fc9d52b0c8365e43d8278fcdd568
Bastian Waidelich <b.waidelich@wwwision.de>
2025-08-23 01:40:07 +0200

    Add CC license (#51)
    * Add CC license
    
    Related: #46
    
    * Update hooks/copyright.py
    
    Co-authored-by: Golo Roden <golo.roden@thenativeweb.io>
    
    ---------
    
    Co-authored-by: Golo Roden <golo.roden@thenativeweb.io>

--- hooks/copyright.py
+++ hooks/copyright.py
@@ -1,3 +1,3 @@
 from datetime import datetime
 def on_config(config, **kwargs):
-    config.copyright = f"Copyright &copy; {datetime.now().year} – <a href=\"/about/\">This site is maintained with passion and commitment by a community of DCB enthusiasts</a>"
+    config.copyright = f"&copy; {datetime.now().year} – Except where otherwise noted, content on this site is licensed under a <a href=\"https://creativecommons.org/licenses/by-sa/4.0/\" target=\"_blank\">Creative Commons Attribution-ShareAlike 4.0 International license</a>"
